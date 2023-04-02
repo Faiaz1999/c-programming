c5dec0ba14365d6fbdc3708e5ab16fa5a8c98b06
Faiaz1999 <apurba15-6090@s.diu.edu.bd>
2023-04-03 02:46:54 +0600

    Module 2

---
+++ Module 2/conditional_2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d",&tk);
+    if(tk >= 100)
+    {
+      printf("Burger Khabo");
+    }
+    else if(tk >= 50)
+    {
+        printf("Fuska Khabo");
+
+    }
+    else if(tk >= 25)
+    {
+        printf("Icecream Khabo");
+    }
+    else
+    {
+        printf("Kchui Khabo Na");
+    }
+
+    return 0;
+}
